a37985b141e5f3dd0c2a14bdef9fd8248f671d0b
Luis Pereira <luis.b.pereira@accenture.com>
2024-06-27 15:28:54 +0100

    Initial program - LP

---
+++ pulumitst_app/index.ts
@@ -0,0 +1,21 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as docker from "@pulumi/docker";
+
+const stack = pulumi.getStack();
+
+// Pull the backend image
+const backendImageName = "backend";
+const backend = new docker.RemoteImage(`${backendImageName}Image`, {
+    name: "pulumi/tutorial-pulumi-fundamentals-backend:latest",
+});
+
+// Pull the frontend image
+const frontendImageName = "frontend";
+const frontend = new docker.RemoteImage(`${frontendImageName}Image`, {
+    name: "pulumi/tutorial-pulumi-fundamentals-frontend:latest",
+});
+
+// Pull the MongoDB image
+const mongoImage = new docker.RemoteImage("mongoImage", {
+    name: "pulumi/tutorial-pulumi-fundamentals-database-local:latest",
+});
